79c0f52e24986e9cbc028f322682e0cb99cb315b
Saurav Kumar Yadav <mems200005041@iiti.ac.in>
2023-09-05 02:21:39 +0530

    Updates

---
+++ precache-manifest.dd6c0e3cfd041a841de98f0140f2f0c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da39675d7947908cde26",
+    "url": "/dealfinder_publish/static/css/main.ed8e8b5d.chunk.css"
+  },
+  {
+    "revision": "da39675d7947908cde26",
+    "url": "/dealfinder_publish/static/js/main.11c67292.chunk.js"
+  },
+  {
+    "revision": "a02a92ec70e619c22c5d",
+    "url": "/dealfinder_publish/static/js/runtime~main.92c1e823.js"
+  },
+  {
+    "revision": "cff71a0957d55b4cbb07",
+    "url": "/dealfinder_publish/static/js/2.b238e6c2.chunk.js"
+  },
+  {
+    "revision": "0818c05112f57344e3fb2e22b95aaec4",
+    "url": "/dealfinder_publish/index.html"
+  }
+];
